41552b2d069005e9f5cbfac1514f83d870a2d322
Jon Fairbanks <fairplay89@gmail.com>
2019-02-20 11:32:02 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ExpressGQL
+
+Express GraphQL Server with Lusca and DDoS Rate Limiting
+
+#### Prerequisites
+- Node v8+
+- Yarn (prefered) or NPM
+- Port 8888 Open and Accessible
+
+#### Quick Start
+- Fetch the code using git or wget
+- While in the app directory, `yarn` or `npm install` to setup
+- Once complete, `yarn start` or `npm start` to launch the server
+- Navigate to <host>:8888 in your browser
+
+*[Optional] To keep ExpressGQL up and running behind the scenes, checkout [PM2](http://pm2.keymetrics.io/ "PM2").*
+
+
+#### Config Options
+The following options can be passed in at runtime as ENV variables:
+- `SESSION_SECRET`: Either a string or array of secrets used to sign the session ID cookie (If array: first is used to sign, others are used to verify)
+- `LOGGING`: If **true**, a graphql.log will be created for incoming site requests using Morgan logging
+- `RATE_LIMIT`: If **true**, enables DDoS and RateLimit protections through Express
+- `PORT`: If set, an alternate port will be used when starting ExpressHTTP. Otherwise, the default of **8888** will be used.
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express'),
+  Ddos = require('ddos'),
+  RateLimit = require('express-rate-limit'),
+  fs = require('fs'),
+  path = require('path'),
+  morgan = require('morgan'),
+  session = require('express-session'),
+  lusca = require('lusca'),
+  graphqlHTTP = require('express-graphql'),
+  { buildSchema } = require('graphql')
+
+const app = express()
+
+// Setup Session Management & Lusca
+if(!process.env.SESSION_SECRET){ console.warn("SESSION_SECRET not passed. Using a default value."); }
+app.use(session({
+    secret: process.env.SESSION_SECRET || "MySessionSecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
+app.use(lusca({
+  csrf: true,
+  csp: false, // Set a valid CSP if desired - https://hacks.mozilla.org/2016/02/implementing-content-security-policy/
+  xframe: 'SAMEORIGIN',
+  hsts: {maxAge: 31536000, includeSubDomains: true, preload: true},
+  xssProtection: true,
+  nosniff: true,
+  referrerPolicy: 'same-origin'
+}))
+
+// Set additional headers and other middlewares if required
+app.use((req, res, next) => {
+  res.removeHeader("X-Powered-By");
+  res.setHeader('X-Timestamp', Date.now()) // Tag all requests with a timestamp
+  res.setHeader('X-Words-of-Wisdom', 'You come at the king, you best not miss. - Omar Little') // Yo dawg...
+  next();
+})
+
+if(process.env.LOGGING == true) {
+    // Create a write stream to log requests (a = append)
+    var accessLogStream = fs.createWriteStream(path.join(__dirname, 'graphql.log'), {flags: 'a'})
+    app.use(morgan('short', {stream: accessLogStream}))
+}
+
+if(process.env.RATE_LIMIT == true) {
+    // Setup DDoS & Rate Limiting
+    const ddos = new Ddos({burst:10, limit:15})
+    const limiter = new RateLimit({
+        windowMs: 15*60*1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+        delayMs: 0 // disable delaying - full speed until the max limit is reached
+    })
+    app.use(ddos.express)
+    app.use('/', limiter)
+}
+
+var port = null;
+if(process.env.PORT){ port = process.env.PORT; }else{ port = 8888; } // Default port is 8888 unless passed
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(port, () => console.log('App Listening on Port ' + port))
